7db8123a0dfd2a3aa29bc543a1629485355c5027
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2022-09-23 01:07:07 +0200

    first commit

---
+++ src/components/Country/Country.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+ // console.log(props.country);
+ const {area, region, population, name, flags} = props.country;
+ // console.log(props.country);
+ return (
+  <div className='country bg-success'>
+   <h2>Country Name: {name.common}</h2>
+   <img src={flags.png} alt="" />
+   <p>Population: {population}</p>
+   <p><small>Region: {region}</small></p>
+   <p><small>Area: {area}</small></p>
+  </div>
+ );
+};
+
+export default Country;
